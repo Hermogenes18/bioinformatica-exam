5568a23edc1022197c526b4499ae8f2a524ffded
Hermogenes18 <hquispemen@unsa.edu.pe>
2021-07-08 12:07:01 -0500

    pagina basica

--- bio.py
+++ bio.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
-import aplicaciones
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-   return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route('/metodos',methods = ['POST', 'GET'])
+
+@app.route("/login",methods=["POST","GET"])
 def login():
-   return render_template()
+    return render_template("index.html")
 
-if __name__ == '__main__':
-   app.run(debug = True)
+
+if __name__ == "__main__":
+   app.run(debug=True)
